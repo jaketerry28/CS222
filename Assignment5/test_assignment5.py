c5ca520b88b20676f25381f433ff8620200d62a7
Jake Terry <47571311+jaketerry28@users.noreply.github.com>
2025-06-05 22:35:11 -0400

    changed assertAlmostEqual to assertEqual

--- Assignment5/test_assignment5.py
+++ Assignment5/test_assignment5.py
@@ -3,11 +3,11 @@ from assignment5 import fahrenheitToCelsius, fibonacci
 
 class TestCalculators(unittest.TestCase):
     def test_fahrenheitToCelsius(self):
-        self.assertAlmostEqual(fahrenheitToCelsius(0), -17.78)
-        self.assertAlmostEqual(fahrenheitToCelsius(32), 0)
-        self.assertAlmostEqual(fahrenheitToCelsius(50), 10)
-        self.assertAlmostEqual(fahrenheitToCelsius(90), 32.22)
-        self.assertAlmostEqual(fahrenheitToCelsius(-15), -26.11)
+        self.assertEqual(fahrenheitToCelsius(0), -17.78)
+        self.assertEqual(fahrenheitToCelsius(32), 0)
+        self.assertEqual(fahrenheitToCelsius(50), 10)
+        self.assertEqual(fahrenheitToCelsius(90), 32.22)
+        self.assertEqual(fahrenheitToCelsius(-15), -26.11)
 
     def test_fibonaci(self):
         with self.assertRaises(ValueError):
